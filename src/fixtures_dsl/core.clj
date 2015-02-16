47fd1ce15b1eee3f08f4df315f8c6413344450a3
Baber Khalil <Baber.s.Khalil@googlemail.com>
2015-02-16 17:04:26 +0000

    [BK] #Tech - started writing a simple grammar.

--- src/fixtures_dsl/core.clj
+++ src/fixtures_dsl/core.clj
@@ -1,6 +1,26 @@
-(ns fixtures-dsl.core)
+(ns fixtures-dsl.core
+  (:require [instaparse.core :as insta])
+)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def whitespace
+  (insta/parser
+    "whitespace = #'\\s+'"))
+
+
+
+
+
+(def fixtures-parse
+  (insta/parser (clojure.java.io/resource "./fixtures_dsl/grammar.txt") :auto-whitespace whitespace )
+)
+
+
+(def data (fixtures-parse (slurp (clojure.java.io/resource "./fixtures_dsl/fixtures.txt"))) )
+
+
+
+
+(:fixture-definition data)
+
+(map #(.toUpperCase (str %1)) parse-tree)
